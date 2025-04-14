d723262c41c3fbb579f6340cf2f81e743810d2e3
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-14 10:44:39 -0400

    Updated Account.jsx

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,1 +1,71 @@
 /* TODO - add your code to create a functional React component that renders account details for a logged in user. Fetch the account data from the provided API. You may consider conditionally rendering a message for other users that prompts them to log in or create an account.  */
+/* TODO - add your code to create a functional React component that renders account details for a logged in user. Fetch the account data from the provided API. You may consider conditionally rendering a message for other users that prompts them to log in or create an account.  */
+import React, { useEffect, useState } from "react"
+const Account = () => {
+    const [user,setUser] = useState(null);
+    const [books, setBooks] = useState([]);
+    useEffect(() => {
+        fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/me", {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Unauthorized or error fetching user details");
+            }
+            return response.json();
+          })
+          .then((data) => setUser(data))
+          .catch((error) => console.error("Error:", error));
+
+        fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/reservations", {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Unauthorized or error fetching reservation details");
+            }
+            return response.json();
+          })
+          .then((data) => setBooks(data))
+          .catch((error) => console.error("Error:", error));
+
+    }, [])
+    if(!user){
+        return (
+            <>
+                <div>Please log in to view your account</div>
+            </>
+        )
+    }
+    return(
+        <div className="accountDetails">
+            <div>
+                <h2>Account Details:</h2>
+            </div>
+            <div>
+                <p>First Name: {user.firstname}</p>
+                <p>Last Name: {user.lastname}</p>
+                <p>Email: {user.email}</p>
+                <p>Id: {user.id}</p>
+            </div>
+            <div>
+                <h2>Checked-out Books</h2>
+                {books.length === 0 ? 
+                    (<p>You have no checked-out books</p>) : 
+                    (<ul>
+                        {books.map((book) => (
+                            <li key={book.id}> {book.title} </li>
+                        ))}
+                    </ul>)
+                }
+            </div>
+        </div>
+    )
+}
+export default Account;
