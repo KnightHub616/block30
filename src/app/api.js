c8162eb8ee77e50f250f027bb6a0092331151a2d
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-14 10:43:35 -0400

    Updated api.js

---
+++ src/app/api.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const API_URL = 'https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api';
+const api = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({
+        baseUrl: API_URL,
+        prepareHeaders: (headers) => {
+            const token = localStorage.getItem("token");
+            if(token){
+                headers.set("Authorization", `Bearer ${token}`)
+            }
+            return headers;
+        }
+    }),
+    tagTypes: ["Books","Users"],
+    endpoints: () => ({}),
+});
+export default api;
